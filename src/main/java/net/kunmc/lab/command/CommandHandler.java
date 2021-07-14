2ca589cb66d5242edb01272ed8f5a936e1edb914
chanmo7na <chanmo7na@gmail.com>
2021-07-14 22:25:12 +0900

    fix 入力コマンドチェックなおし

--- src/main/java/net/kunmc/lab/command/CommandHandler.java
+++ src/main/java/net/kunmc/lab/command/CommandHandler.java
@@ -37,33 +37,53 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
             return true;
         }
 
-        if(1 != args.length) {
-            sender.sendMessage(ChatColor.RED + "usage: ¥naging <" + CommandConst.COMMAND_START + "|" + CommandConst.COMMAND_STOP + ">" );
-        }
-
         if(null == this.plugin) {
             return true;
         }
 
         switch (args[0]) {
             case CommandConst.COMMAND_START:
+                if(!(args.length == 1)) {
+                    sender.sendMessage(ChatColor.RED + "usage: \n/aging <" + CommandConst.COMMAND_START + " | " + CommandConst.COMMAND_STOP + ">");
+                    return true;
+                }
                 this.plugin.startGame();
                 sender.sendMessage(ChatColor.GREEN + "info: 老化プラグイン が有効化されました");
                 break;
             case CommandConst.COMMAND_STOP:
+                if(!(args.length == 1)) {
+                    sender.sendMessage(ChatColor.RED + "usage: \n/aging <" + CommandConst.COMMAND_START + " | " + CommandConst.COMMAND_STOP + ">");
+                    return true;
+                }
                 this.plugin.stopGame();
                 sender.sendMessage(ChatColor.GREEN + "info: 老化プラグイン が無効化されました");
                 break;
             case CommandConst.COMMAND_SUSPEND:
+                if(!(args.length == 1)) {
+                    sender.sendMessage(ChatColor.RED + "usage: \n/aging <" + CommandConst.COMMAND_SUSPEND + " | " + CommandConst.COMMAND_RESTART + ">");
+                    return true;
+                }
                 this.plugin.suspend();
                 sender.sendMessage(ChatColor.GREEN + "info: 老化プラグイン が一時停止されました");
                 break;
             case CommandConst.COMMAND_RESTART:
+                if(!(args.length == 1)) {
+                    sender.sendMessage(ChatColor.RED + "usage: \n/aging <" + CommandConst.COMMAND_SUSPEND + " | " + CommandConst.COMMAND_RESTART + ">");
+                    return true;
+                }
                 this.plugin.restart();
                 sender.sendMessage(ChatColor.GREEN + "info: 老化プラグイン が再開されました");
                 break;
             case CommandConst.COMMAND_CONF:
-                onConf(args);
+                String errorMessage = checkArgs(args);
+                if(!errorMessage.isEmpty()){
+                    sender.sendMessage(ChatColor.RED + "error: " + errorMessage);
+                    break;
+                }
+                errorMessage = setConf(args);
+                if(!errorMessage.isEmpty()) {
+                    sender.sendMessage(ChatColor.GREEN + "info: " + errorMessage);
+                }
                 break;
             case CommandConst.COMMAND_SET:
                 break;
@@ -149,52 +169,73 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
         return getPlayerName().anyMatch(e -> e.equals(args));
     }
 
-    public String onConf(String[] args) {
-        // 1年経過するのに必要なtrik数を変更する
+    private String checkArgs(String[] args) {
+        if(args.length < 3) {
+            return "引数が足りません";
+        }
         if(CommandConst.ARGS1_PERIOD.equals(args[1])) {
-            if( !(4 == args.length) ) {
+            if(args.length > 4) {
+                return "引数が多すぎます";
+            }
+            int trik = Integer.parseInt(args[2]);
+            if(trik < CommandConst.MIN_WALKSPEED || CommandConst.MAX_WALKSPEED < trik) {
+                return "trikは" + CommandConst.MIN_WALKSPEED + "〜" + CommandConst.MAX_WALKSPEED + "の間で設定してください";
+            }
+        }
+        // TODO: コマンドが間違っています
+
+        if(isGenerationName(args[1])) {
+            if(args.length < 4) {
                 return "引数が足りません";
             }
-
-            int trik = Integer.parseInt(args[3]);
-            if(trik < CommandConst.MIN_WALKSPEED || CommandConst.MAX_WALKSPEED < trik) {
-                return "trikは" + CommandConst.MIN_WALKSPEED + "~" + CommandConst.MAX_WALKSPEED + "の間で設定してください";
+            if(5 <= args.length) {
+                return "引数が多すぎます";
             }
+            switch (args[2]) {
+                case CommandConst.ARGS2_WALKSPEED:
+                    // TODO: 例外処理の追加
+                    int walkSpeed = Integer.parseInt(args[3]);
+                    if(walkSpeed < -1 || 1 < walkSpeed) {
+                        return "歩行速度は-1.0~1.0の間で設定してください";
+                    }
+                    break;
+                case CommandConst.ARGS2_MAXHP:
+                    int maxHp = Integer.parseInt(args[3]);
+                    if(maxHp < 1 || 10 < maxHp) {
+                        return "最大HPは1~10の間で設定してください";
+                    }
+                    break;
+                case CommandConst.ARGS2_FOODLEVEL:
+                    int foodLevel = Integer.parseInt(args[3]);
+                    if(foodLevel < 1 || 10 < foodLevel) {
+                        return "空腹度は1~10の間で設定してください";
+                    }
+                    break;
+            }
+        }
+        return "";
+    }
 
+    public String setConf(String[] args) {
+        // 1年経過するのに必要なtrik数を変更する
+        if(CommandConst.ARGS1_PERIOD.equals(args[1])) {
+            int trik = Integer.parseInt(args[2]);
             plugin.setConfig(ConfigConst.PERIOD, trik);
-            return "1年経過するのに必要なtrik数を" + trik + "に変更しました";
+            return "1年経過するのに必要なtrik数を " + trik + " に変更しました";
         }
 
-        if( !Generation.Type.BABY.name.equals(args[1])
-        && !Generation.Type.KIDS.name.equals(args[1])
-        && !Generation.Type.YOUNG.name.equals(args[1])
-        && !Generation.Type.ADULT.name.equals(args[1])
-        && !Generation.Type.ELDERLY.name.equals(args[1]) ) {
-            return "世代指定が間違っています";
-        }
-
-        // 歩行速度、最大HP、空腹値の最大値を変更する
+        // 各世代の歩行速度、最大HP、空腹値の最大値を変更する
         switch (args[2]) {
             case CommandConst.ARGS2_WALKSPEED:
-                // TODO: 例外処理の追加
                 int walkSpeed = Integer.parseInt(args[3]);
-                if(walkSpeed < -1 || 1 < walkSpeed) {
-                    return "歩行速度は-1.0~1.0の間で設定してください";
-                }
                 plugin.setConfig(args[1] + "." + ConfigConst.WALK_SPEED, walkSpeed);
                 break;
             case CommandConst.ARGS2_MAXHP:
                 int maxHp = Integer.parseInt(args[3]);
-                if(maxHp < 1 || 10 < maxHp) {
-                    return "最大HPは1~10の間で設定してください";
-                }
                 plugin.setConfig(args[1] + "." + ConfigConst.MAX_HP, maxHp);
                 break;
             case CommandConst.ARGS2_FOODLEVEL:
                 int foodLevel = Integer.parseInt(args[3]);
-                if(foodLevel < 1 || 10 < foodLevel) {
-                    return "空腹度は1~10の間で設定してください";
-                }
                 plugin.setConfig(args[1] + "." + ConfigConst.FOOD_LEVEL, foodLevel);
                 break;
         }
